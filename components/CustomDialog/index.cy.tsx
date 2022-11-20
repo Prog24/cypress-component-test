eb7ac753980c376f85aea1402d23e1ed2f0e5bab
Kenta Suzuki <kenta@suzuki.dev>
2022-11-21 08:43:35 +0900

    fix: muiのカラーをテストで利用できるように修正 (#3)

--- components/CustomDialog/index.cy.tsx
+++ components/CustomDialog/index.cy.tsx
@@ -1,5 +1,15 @@
 import { useState } from 'react'
+import { blue } from '@mui/material/colors'
 import CustomDialog from './index'
+
+// TODO: utilsか何かに切り出す
+const hex2rgb = (hex: string) => {
+  hex = hex.slice(1)
+  const rgb = [hex.slice(0, 2), hex.slice(2, 4), hex.slice(4, 6)].map((str) => {
+    return parseInt(str, 16)
+  })
+  return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
+}
 
 const CheckDialog = () => {
   const [open, setOpen] = useState<boolean>(true);
@@ -18,7 +28,7 @@ describe('<CustomDialog>', () => {
   it('mounts', () => {
     cy.mount(<CheckDialog />)
     cy.findByTestId('title').should('have.text', 'ダイアログタイトル')
-    cy.findByTestId('title').should('have.css', 'color', 'rgb(25, 118, 210)')
+    cy.findByTestId('title').should('have.css', 'color', `${hex2rgb(blue[700])}`)
     cy.findByTestId('submit').click()
   })
 })
