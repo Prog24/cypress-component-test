5a99565e67a1e6a9f76007a7a3aff8924e8eb4c8
Prog24 <kenta@suzuki.dev>
2022-11-24 15:22:20 +0900

    fix: ThemeProviderの設定

--- components/CustomText/index.cy.tsx
+++ components/CustomText/index.cy.tsx
@@ -1,9 +1,16 @@
 import CustomText from './index'
+import { teal } from '@mui/material/colors'
+import ThemeProvider from '../../ThemeProvider'
+import { hexToRgb } from '@mui/material'
 
 describe('<CustomText>', () => {
   it('mounts', () => {
-    cy.mount(<CustomText>テキスト</CustomText>)
+    cy.mount(
+      <ThemeProvider>
+        <CustomText>テキスト</CustomText>
+      </ThemeProvider>
+    )
     cy.findByTestId('hoge-text').should('have.text', 'テキスト')
-    cy.findByTestId('hoge-text').should('have.css', 'color', 'rgb(25, 118, 210)')
+    cy.findByTestId('hoge-text').should('have.css', 'color', `${hexToRgb(teal[500])}`)
   })
 })
