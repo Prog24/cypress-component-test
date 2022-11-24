44b3a4ed9c379a13fbf0cf27681e9988e3241999
Prog24 <kenta@suzuki.dev>
2022-11-24 12:51:04 +0900

    fix: snapshotのサイズ変更

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,6 +6,13 @@ export default defineConfig({
   component: {
     setupNodeEvents(on, config) {
       addMatchImageSnapshotPlugin(on, config)
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name == 'chrome' && browser.isHeadless) {
+          launchOptions.args.push('--window-size=500,500')
+          launchOptions.args.push('--force-device-scale-factor=1')
+        }
+        return launchOptions
+      })
     },
     viewportWidth: 500,
     viewportHeight: 500,
